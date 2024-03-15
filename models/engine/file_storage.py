fe7559497bfcc59b7bf993fc15e33ab3c062685f
A-asava <adikasaid996@gmail.com>
2024-03-15 13:08:51 +0000

    debuging

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,58 +45,9 @@ class FileStorage:
         """Returns a dictionary of classes"""
 
         from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
 
         return {
                 "BaseModel": BaseModel,
-                "User": User,
-                "State": State,
-                "City": City,
-                "Amenity": Amenity,
-                "Place": Place,
-                "Review": Review
-                }
-
-    def attributes(self):
-        """Returns the valid attributes and their types"""
-
-        return {"BaseModel":
-                {"id": str,
-                    "created_at": datetime.datetime,
-                    "updated_at": datetime.datetime},
-                "User":
-                {"email": str,
-                    "password": str,
-                    "first_name": str,
-                    "last_name": str},
-                "State":
-                {"name": str},
-                "City":
-                {"state_id": str,
-                    "name": str},
-                "Amenity":
-                {"name": str},
-                "Place":
-                {"city_id": str,
-                    "user_id": str,
-                    "name": str,
-                    "description": str,
-                    "number_rooms": int,
-                    "number_bathrooms": int,
-                    "max_guest": int,
-                    "price_by_night": int,
-                    "latitude": float,
-                    "longitude": float,
-                    "amenity_ids": list},
-                "Review":
-                {"place_id": str,
-                    "user_id": str,
-                    "text": str}
                 }
 
     def reload(self):
